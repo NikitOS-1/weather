6d100a17cb87f7193d6ddd7ece779776e59fb7ff
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-04-25 23:09:17 +0300

    .

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,4 +1,15 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
+
 export default App;
