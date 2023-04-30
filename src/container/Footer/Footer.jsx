8df36b89db0b12d069bde564903dbc902332d118
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-05-01 00:54:41 +0300

    compl footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,5 +1,10 @@
+import style from "./Footer.module.scss";
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className={style.after}>© 2023 Private Todos. Nikita Puzyrenko</div>
+    </footer>
+  );
 };
 
 export default Footer;
